a6d62cd82d171ffa6a664c4d634001bbd4235354
Anton Starostin <evil-cat@LokaLoka.localdomain>
2019-11-30 14:56:18 +0200

    button

--- src/StartPage.js
+++ src/StartPage.js
@@ -1,9 +1,24 @@
 import React from "react";
 import "./App.css";
+import { Link } from "react-router-dom";
 
 class StartPage extends React.Component {
   render() {
-    return <div>Start Page</div>;
+    return (
+      <div>
+        Start Page
+        <button to="/app">Start</button>
+        <Col xs="6" className="text-right">
+          <Link to="/app" className="btn">
+            Start
+          </Link>
+        </Col>
+        <Link to="/app" className="btn">
+          Start
+        </Link>
+        {/* <button onClick={App}>Start</button> */}
+      </div>
+    );
   }
 }
 
--- src/index.js
+++ src/index.js
@@ -3,16 +3,18 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 import * as serviceWorker from "./serviceWorker";
-import { Switch, Route } from "react-router-dom";
+import { BrowserRouter, Switch, Route } from "react-router-dom";
 import StartPage from "./StartPage";
 import FinalPage from "./FinalPage";
 
 ReactDOM.render(
-  <Switch>
-    <Route exact path="/startPage" content={StartPage} />
-    <Route exact path="/App" content={App} />
-    <Route exact path="/finalPage" content={FinalPage} />
-  </Switch>,
+  <BrowserRouter>
+    <Switch>
+      <Route exact path="/app" component={App} />
+      <Route exact path="/" component={StartPage} />
+      <Route exact path="/finalPage" component={FinalPage} />
+    </Switch>
+  </BrowserRouter>,
   document.getElementById("root")
 );
 
